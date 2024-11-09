9ef657a12a2c128628fafefeba2dc6813dcb9839
Ваше ім'я <constantin161089@gmail.com>
2024-11-09 09:14:59 +0200

    writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+
+    await fs.writeFile(PATH_DB, data, 'utf8');
+
+    console.log('Contacts have been written to the file successfully.');
+  } catch (error) {
+    console.error('Failed to write contacts to the file:', error);
+  }
+};
