737924a1c363caea0658a0326b1f2073ddd38c97
Ваше ім'я <constantin161089@gmail.com>
2024-11-09 09:09:15 +0200

    contacts,writeContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'fs/promises';
 
-export const readContacts = async () => {};
+export async function readContacts() {
+  const contacts = await fs.readFile(PATH_DB, 'utf-8');
+  return JSON.parse(contacts);
+}
