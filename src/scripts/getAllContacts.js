518a99fdf9cafda94b093c2111ea62736142904d
Ваше ім'я <constantin161089@gmail.com>
2024-11-11 12:44:12 +0200

    get contacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contactsAll = await readContacts();
+    return contactsAll;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await getAllContacts());
