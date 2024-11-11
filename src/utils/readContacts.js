56393d81bd0d962ac4baff0c7179a4aa34057c97
Ваше ім'я <constantin161089@gmail.com>
2024-11-11 12:15:15 +0200

    update read,write

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,7 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'fs/promises';
 
-export async function readContacts() {
-  const contacts = await fs.readFile(PATH_DB, 'utf-8');
-  return JSON.parse(contacts);
-}
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, 'utf-8');
+
+    return JSON.parse(contacts) || [];
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -2,13 +2,10 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
+  const contacts = JSON.stringify(updatedContacts, null, 2);
   try {
-    const data = JSON.stringify(updatedContacts, null, 2);
-
-    await fs.writeFile(PATH_DB, data, 'utf8');
-
-    console.log('Contacts have been written to the file successfully.');
+    await fs.writeFile(PATH_DB, contacts, 'utf8');
   } catch (error) {
-    console.error('Failed to write contacts to the file:', error);
+    console.log(error);
   }
 };
