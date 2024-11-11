ce0b0cac2bc092a56cf5a5de02df641a93daf651
Ваше ім'я <constantin161089@gmail.com>
2024-11-11 12:21:05 +0200

    generate contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,21 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+
+const generateContacts = async (number) => {
+  try {
+    const allContacts = await readContacts();
+
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...allContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 generateContacts(5);
